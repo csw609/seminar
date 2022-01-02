88fd513db7b13ac81c3b1c378e1e9879d112573c
csw609 <csw609@naver.com>
2022-01-02 11:39:48 +0900

    final

--- src/seminar_pub.cpp
+++ src/seminar_pub.cpp
@@ -1,24 +1,25 @@
-#include "ros/ros.h"
-#include "std_msgs/String.h"
+#include "ros/ros.h"              // ROS basic header
+#include "std_msgs/String.h"      // std_msgs package
 
 int main(int argc, char **argv)
 {
-  ros::init(argc, argv, "seminar_pub");
-  ros::NodeHandle nh;
+  ros::init(argc, argv, "seminar_pub");     //initialize name of node
+  ros::NodeHandle nh;                       //Declare node handle for communication with ROS system
 
-  ros::Publisher chatter_pub = nh.advertise<std_msgs::String>("messages", 1000);
-
-  ros::Rate loop_rate(10);
+  ros::Publisher chatter_pub = nh.advertise<std_msgs::String>("messages", 1000);  // Declare Publisher
+                                                                                  // topic name : messages
+                                                                                  // queue size : 1000
+  ros::Rate loop_rate(10);    // operate 10HZ
   while (ros::ok())
   {
-    std_msgs::String msg;
-    msg.data = "hello world";
-    ROS_INFO("Publish Msgs");
-    chatter_pub.publish(msg);
+    std_msgs::String msg;     // declare message for publish
+    msg.data = "hello world"; // contents of message
 
-    ros::spinOnce();
+    ROS_INFO("Publish Msgs");//print to console
 
-    loop_rate.sleep();
+    chatter_pub.publish(msg); // publish messages
+
+    loop_rate.sleep();  // sleep for operation frequency
   }
 
   return 0;
--- src/seminar_sub.cpp
+++ src/seminar_sub.cpp
@@ -1,9 +1,10 @@
 #include "ros/ros.h"
 #include "std_msgs/String.h"
 
+//Callback function that works when a message is received.
 void chatterCallback(const std_msgs::String::ConstPtr& msg)
 {
-  ROS_INFO("%s", msg->data.c_str());
+  ROS_INFO("%s", msg->data.c_str());  //print a message to console
 }
 
 int main(int argc, char **argv)
@@ -13,7 +14,8 @@ int main(int argc, char **argv)
 
   ros::Subscriber sub = nh.subscribe("messages", 1000, chatterCallback);
 
-  ros::spin();
+  ros::spin(); // Function waits to receive a message.
+               // When a message is received, the callback function is called.
 
   return 0;
 }
